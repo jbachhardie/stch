eea1a33dcaad8c92d3643c7655a5dea001411401
Jae Bach Hardie <jhardie@findmypast.com>
2018-02-05 17:19:50 +0000

    :recycle: Update Todo to use graphqlyoga

--- packages/todo/src/server.js
+++ packages/todo/src/server.js
@@ -1,19 +1,20 @@
-const { microGraphiql, microGraphql } = require('apollo-server-micro');
-const micro = require('micro');
-const { send } = micro;
-const { get, post, router } = require('microrouter');
-const schema = require('./schema');
+const { GraphQLServer } = require('graphql-yoga');
+const db = require('./db');
 
-const graphqlHandler = microGraphql({ schema });
-const graphiqlHandler = microGraphiql({ endpointURL: '/graphql' });
+const typeDefs = 'schema.graphql';
+const resolvers = {
+  Query: {
+    todosByAuthor: (_obj, { authorId }) => db.getTodosByAuthor(authorId),
+  },
+  Mutation: {
+    createTodo: (_obj, { authorId, title }) => db.addTodo({ authorId, title }),
+    completeTodo: (_obj, { id }) => db.complete(id),
+    uncompleteTodo: (_obj, { id }) => db.uncomplete(id),
+  },
+};
 
-const server = micro(
-  router(
-    get('/graphql', graphqlHandler),
-    post('/graphql', graphqlHandler),
-    get('/graphiql', graphiqlHandler),
-    (req, res) => send(res, 404, 'not found')
-  )
+const server = new GraphQLServer({ typeDefs, resolvers });
+const port = 6002;
+server.start({ port }, () =>
+  console.log('Server is running on localhost:', port)
 );
-
-server.listen(6002);
