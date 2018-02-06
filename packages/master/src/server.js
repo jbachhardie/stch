17b887cc565ddb8976385caaac3d71f4f9ec143e
Jae Bach Hardie <jhardie@findmypast.com>
2018-02-06 09:56:07 +0000

    :sparkles: Schemata stitched in master

--- packages/master/src/server.js
+++ packages/master/src/server.js
@@ -1,3 +1,4 @@
+const fp = require('lodash/fp');
 const { GraphQLServer } = require('graphql-yoga');
 const fetch = require('node-fetch');
 const { HttpLink } = require('apollo-link-http');
@@ -6,12 +7,75 @@ const {
   makeRemoteExecutableSchema,
   mergeSchemas,
 } = require('graphql-tools');
+const port = 6060;
 
-const userLink = new HttpLink({ uri: 'http://localhost:6001', fetch });
-const userSchema = await introspectSchema(userLink)
+const schemaExtensions = `
+  extend type User {
+    todos: [Todo]
+  }
 
-const server = new GraphQLServer({ schema });
-const port = 6000;
-server.start({ port }, () =>
-  console.log('Server is running on localhost:', port)
+  extend type Todo {
+    author: User
+  }
+`;
+
+const resolverExtensions = mergeInfo => ({
+  User: {
+    todos: {
+      fragment: `fragment UserFragment on User { id }`,
+      resolve(parent, args, context, info) {
+        const authorId = parent.id;
+        return mergeInfo.delegate(
+          'query',
+          'todosByAuthor',
+          {
+            authorId,
+          },
+          context,
+          info
+        );
+      },
+    },
+  },
+  Todo: {
+    author: {
+      fragment: `fragment TodoFragment on Todo { authorId }`,
+      resolve(parent, args, context, info) {
+        const id = parent.authorId;
+        return mergeInfo.delegate(
+          'query',
+          'user',
+          {
+            id,
+          },
+          context,
+          info
+        );
+      },
+    },
+  },
+});
+
+const fetchRemoteSchemas = fp.flow(
+  fp.map(uri => new HttpLink({ uri, fetch })),
+  fp.map(async link => ({ schema: await introspectSchema(link), link })),
+  fp.map(async data => makeRemoteExecutableSchema(await data)),
+  schemas => Promise.all(schemas)
 );
+
+const main = async () => {
+  const remoteSchemas = await fetchRemoteSchemas([
+    'http://localhost:6001/',
+    'http://localhost:6002/',
+  ]);
+  const schema = mergeSchemas({
+    schemas: [...remoteSchemas, schemaExtensions],
+    resolvers: resolverExtensions,
+  });
+  const server = new GraphQLServer({ schema });
+  server.start({ port }, () =>
+    console.log('Server is running on localhost:', port)
+  );
+};
+
+main().catch(console.error);
